737c9c448a5640a4926525ee9d5535244e932674
Ivan Tsar <ivan.tsar99@gmail.com>
2019-11-12 22:22:05 +0300

    program with classes Cell and MyException

---
+++ Cell/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Input your cell:");
+                char column = char.Parse(Console.ReadLine().ToUpper());
+                int row = int.Parse(Console.ReadLine());
+
+                try
+                {
+                    Cell cell = new Cell(column, row);
+                    break;
+                }
+                catch (NotCorrectInputExeption exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+                catch (Exception)
+                {
+                }
+            }
+        }
+    }
+}
