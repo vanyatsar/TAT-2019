c9f6ac1da85b6c19377f4ab17fd536aa5f9c94ac
Ivan Tsar <ivan.tsar99@gmail.com>
2019-11-12 21:56:32 +0300

    implementation of the algorithm for counting repetitions in a row

---
+++ Homework1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input word: ");
+            string name = Console.ReadLine();
+
+            int count = 1;
+            int maxCount = 0;
+            int length = name.Length;
+
+            for (int i = 0; i < length - 1; i++)
+            {
+                if (name[i] == name[i + 1])
+                {
+                    count++;
+                    if (maxCount < count)
+                    {
+                        maxCount = count;
+                    }
+                }
+                else
+                {
+                    count = 1;
+                }
+            }
+            Console.WriteLine($"Answer: {maxCount}");
+        }
+    }
+}
