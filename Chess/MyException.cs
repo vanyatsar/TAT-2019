07bd9a1f1b6339ff1634cd0bef4a6fca87236bba
Ivan Tsar <ivan.tsar99@gmail.com>
2019-11-13 01:34:54 +0300

    add classes Cell.cs MyException.cs; realize input coordinates and input check

---
+++ Chess/MyException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+    public class NotCorrectInputException : Exception
+    {
+        public NotCorrectInputException(string message) : base(message) { }
+    }
+}
