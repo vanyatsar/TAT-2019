8fd7e3306bbd3f315c945bcb053eb44358cd4ef0
Ivan Tsar <ivan.tsar99@gmail.com>
2019-11-24 15:44:29 +0300

    add login to mail.ru

--- DEV-4/Program.cs
+++ DEV-4/Program.cs
@@ -13,6 +13,25 @@ namespace DEV_4
     {
         static void Main(string[] args)
         {
-        } 
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            driver.Navigate().GoToUrl("https://mail.ru/");
+            driver.Manage().Window.Maximize();
+
+            Input(driver);
+
+            //driver.Quit();
+        }
+
+        static void Input(IWebDriver driver)
+        {
+            IWebElement searchLogin = driver.FindElement(By.Id("mailbox:login"));
+            searchLogin.SendKeys("tat2019.2" + Keys.Enter);
+
+            IWebElement searchPassword = driver.FindElement(By.Id("mailbox:password"));
+            searchPassword.SendKeys("verydifficult" + Keys.Enter);
+        }
     }
 }
