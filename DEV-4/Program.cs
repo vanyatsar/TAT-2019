0edd9dafc9b9b0a9fb64c98d7015f92e49e971c4
Ivan Tsar <ivan.tsar99@gmail.com>
2019-11-24 15:41:34 +0300

    create empty project

---
+++ DEV-4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+/// <summary>
+/// log: tat2019.2
+/// pass: verydifficult
+/// </summary>
+
+namespace DEV_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        } 
+    }
+}
