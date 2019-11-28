3610b1b66e5fe73578743c868521b78d8e93f7f6
Ivan Tsar <ivan.tsar99@gmail.com>
2019-11-28 17:18:16 +0300

    add new class StartMailPage.cs in namespace DEV_5.PageObjects; realize login to mail

---
+++ DEV-5/PageObjects/StartMailPage.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace DEV_5.PageObjects
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class LoginMailPage
+    {
+        private readonly By usernameLocator = By.Id("mailbox:login");
+        private readonly By passwordLocator = By.Id("mailbox:password");
+
+        IWebElement element;
+        IWebElement button;
+
+        private WebDriverWait _waitTime;
+        private readonly IWebDriver _driver;
+
+        public LoginMailPage(IWebDriver driver)
+        {
+            _driver = driver;
+            _waitTime = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        public LoginMailPage NavigetToLoginPage()
+        {
+            _driver.Manage().Window.Maximize();
+            _driver.Navigate().GoToUrl("https://mail.ru/");
+            return this;
+        }
+
+        public LoginMailPage TypeUsename(string username)
+        {
+            GetWebElement(usernameLocator).SendKeys(username + Keys.Enter);
+            return this;
+        }
+
+        public LoginMailPage TypePassword(string password)
+        {
+            GetWebElement(passwordLocator).SendKeys(password + Keys.Enter);
+            return this;
+        }
+
+        private IWebElement GetWebElement(By locator)
+        {
+            element = _waitTime
+                .Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(locator));
+            return element;
+        }
+
+        public void LoginToMail(string username, string password)
+        {
+            TypeUsename(username);
+            TypePassword(password);
+
+        }
+
+    }
+}
--- DEV-5/Program.cs
+++ DEV-5/Program.cs
@@ -1,15 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using DEV_5.PageObjects;
 
 namespace DEV_5
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        static void Main()
+        { 
+            const string LOGIN_USERNAME = "tat2019.2";
+            const string LOGIN_PASSWORD = "verydifficult";
+
+            IWebDriver driver = new ChromeDriver();
+            LoginMailPage loginPage = new LoginMailPage(driver);
+
+            loginPage.NavigetToLoginPage();
+            loginPage.LoginToMail(LOGIN_USERNAME, LOGIN_PASSWORD);
         }
     }
 }
