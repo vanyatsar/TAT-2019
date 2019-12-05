0ca852990ed220cb61e840524a67e6e4aafceb57
Ivan Tsar <ivan.tsar99@gmail.com>
2019-12-05 10:11:18 +0300

    add new class MailManager.cs

--- DEV-5/PageObjects/MailRuMainPage.cs
+++ DEV-5/PageObjects/MailRuMainPage.cs
@@ -4,10 +4,9 @@ namespace DEV_5.PageObjects
 {
     public class MailRuMainPage : BasePage
     {
-        private readonly By textMessageLocator = By.XPath("/html/body/div[17]/div[2]/div/div[1]/div[2]/div[3]/div[5]/div/div/div[2]/div[1]/div");
+        private readonly By textMessageLocator = By.XPath("//div[contains(@tabindex, \"505\")]");
         private readonly By destinationLocator = By.CssSelector(".container--zU301");
         private readonly By buttonLocator = By.ClassName("compose-button__wrapper");
-        private readonly By buttonSendLocator = By.ClassName("button2__txt");
 
         public MailRuMainPage(IWebDriver driver) : base (driver)
         {
@@ -27,23 +26,21 @@ namespace DEV_5.PageObjects
   
         private MailRuMainPage TextLetter(string text)
         {
-            GetWebElement(textMessageLocator).SendKeys(text+ Keys.Control + Keys.Enter);
+            GetWebElement(textMessageLocator).SendKeys(text + Keys.Control + Keys.Enter);
             return this;
         }
-
+        /*
         private MailRuMainPage ClickToSendButton()
         {
             GetWebElement(buttonSendLocator);
             return this;
         }
-
+        */
         public void SendMessage(string email, string textOfLetter)
         {
             ClickToPrintButton();
             InputDestination(email);
             TextLetter(textOfLetter);
-            ClickToSendButton();
-            //this.SendLetterButton.Click();
         }
 
     }
